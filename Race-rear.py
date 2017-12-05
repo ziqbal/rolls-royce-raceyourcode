3cb0ec3dea765f5d5f7690cbba09c238951e4488
Zafar Iqbal <ultrasine@gmail.com>
2017-12-05 14:38:53 +0200

    gitup

--- Race-rear.py
+++ Race-rear.py
@@ -2,53 +2,12 @@
 
 WaitForGo( )
 
-AimForLane( -1 )
-Speed( 100 )
+AimForLane( 0 )
+Speed( 90 )
 
 while Globals.running :
 
-    WaitForWaypoint( 2 )
-
-    AimForLane( 1 )
-    Speed( 80 )
-
-
-    WaitForWaypoint( 3 )
-
-    AimForLane( -1 )
-    Speed( 100 )
-
-
-    WaitForWaypoint( 6 )
-
-    AimForLane( 1 )
-    Speed( 100 )
-
-    WaitForWaypoint( 9 )
-
-    AimForLane( -1 )
-    Speed( 100 )
-
-
-    #WaitForWaypoint( 2 )
-    #Speed( 70 )
-    #AimForLane( 2 )
-
-
-    #WaitForWaypoint( 3 )
-    #AimForLane( 0 )
-    #Speed( 100 )
-
-    #WaitForWaypoint( 4 )
-    #Speed(80)
-    #AimForLane( 1 )
-
-
-
-
-    #WaitForWaypoint( 9 )
-    ##Speed(100)
-    #AimForLane( 0 )
+    AimForLane( random.uniform(-1,1 )
 
     WaitForSeconds( 0.01 )
 
