acb11344f51f7599d408bb2168539d3d615f4167
Zafar Iqbal <ultrasine@gmail.com>
2017-12-05 14:25:43 +0200

    gitup

--- Race-front.py
+++ Race-front.py
@@ -1,11 +1,15 @@
 
-AimForLane( 0 )
+AimForLane( 1.5 )
 Speed( 100 )
 
 WaitForGo( )
 
-
 while Globals.running :
+
+    WaitForWaypoint( 2 )
+    AimForLane( 1.5 )
+    Speed( 100 )
+
     WaitForSeconds( 0.01 )
     pass
 
